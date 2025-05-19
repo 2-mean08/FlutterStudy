6ca2ba505afc8a5ba8cc0a592323db7a29a5869e
2-mean08 <leejm010813@gmail.com>
2025-05-19 23:14:39 +0900

    Delete lib directory

--- lib/page3.dart
+++
@@ -1,9 +0,0 @@
-import 'package:a_7_1/main.dart';
-import 'package:flutter/material.dart';
-
-class Page3 extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Text('page3');
-  }
-}
